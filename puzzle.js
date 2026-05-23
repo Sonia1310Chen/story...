34786aab7382b8260462f984080395e2ba385127
Naomi <yuniceng45@gmail.com>
2026-05-23 15:50:57 +0800

    Create puzzle.js

---
+++ puzzle.js
@@ -0,0 +1,83 @@
+let board = [
+    1,2,3,
+    4,0,6,
+    7,5,8
+];
+
+const correctBoard = [
+    1,2,3,
+    4,5,6,
+    7,8,0
+];
+
+function showPuzzle(){
+
+    const msg = document.getElementById("puzzlelock");
+
+    if(msg.style.display === "block"){
+
+        msg.style.display = "none";
+
+    }
+    else{
+
+        msg.style.display = "block";
+
+        drawPuzzle();
+
+    }
+
+}
+function moveSteamer(index){
+
+    let empty = board.indexOf(0);
+
+    if(canMove(index, empty)){
+
+        let temp = board[index];
+
+        board[index] = 0;
+
+        board[empty] = temp;
+
+        drawPuzzle();
+
+        checkPuzzle();
+
+    }
+
+}
+function canMove(index, empty){
+
+    if(index == empty-1 && empty%3 != 0)return true;
+
+    if(index == empty+1 && index%3 != 0)return true;
+
+    if(index == empty-3)return true;
+
+    if(index == empty+3)return true;
+
+    return false;
+
+}
+function drawPuzzle(){
+
+    for(let i=0;i<9;i++){
+
+        let tile =
+        document.getElementById("s"+i);
+
+        if(board[i] == 0){
+
+            tile.innerText = "";
+
+        }
+        else{
+
+            tile.innerText = board[i];
+
+        }
+
+    }
+
+}
