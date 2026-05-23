cea21ef91363f1787f999f5a47cb09c7e589d894
Sonia <104069soniachen@gmail.com>
2026-05-23 22:25:30 +0800

    added streamer's puzzle to html and js
    added her work or the puzzle game
    but things after the streamer haven't been connected yet
    and the check of the puzzle?

--- script.js
+++ script.js
@@ -385,7 +385,6 @@ function toroom() {
     document.getElementById("bedroomscene").style.display = "block";
 }
 function takekey() {
-    
     document.getElementById("screen1").style.display = "none";
     document.getElementById("moldpass").style.display = "none";
     document.getElementById("keyk1").style.display = "none";
@@ -394,7 +393,89 @@ function takekey() {
     document.getElementById("pickupckey").style.display = "none";
 }
 
+let board = [
+    1,3,6,
+    5,0,2,
+    4,7,8
+];
 
+const correctBoard = [
+    1,2,3,
+    4,5,6,
+    7,8,0
+];
+
+function showPuzzle(){
+
+    const msg = document.getElementById("puzzlelock");
+
+    if(msg.style.display === "block"){
+        document.getElementById("puzzle").style.display = "none";
+        msg.style.display = "none";
+
+    }
+    else{
+
+        msg.style.display = "block";
+        document.getElementById("puzzle").style.display = "block";
+        drawPuzzle();
+
+    }
+
+}
+function moveSteamer(index){
+
+    let empty = board.indexOf(0);
+
+    if(canMove(index, empty)){
+
+        let temp = board[index];
+
+        board[index] = 0;
+
+        board[empty] = temp;
+
+        drawPuzzle();
+
+        checkPuzzle();
+
+    }
+
+}
+function canMove(index, empty){
+
+    if(index == empty-1 && empty%3 != 0)return true;
+
+    if(index == empty+1 && index%3 != 0)return true;
+
+    if(index == empty-3)return true;
+
+    if(index == empty+3)return true;
+
+    return false;
+
+}
+function drawPuzzle(){
+
+    for(let i=0;i<9;i++){
+
+        let tile =
+        document.getElementById("s"+i);
+
+        if(board[i] == 0){
+
+            tile.innerText = "";
+
+        }
+        else{
+
+            tile.innerText = board[i];
+
+        }
+
+    }
+
+}
 
 nextBtn.addEventListener('click', () => {
     currentStep++;
